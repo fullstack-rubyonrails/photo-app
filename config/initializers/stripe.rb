92a047b7ca75f62a9b7b7ce174cf4f023543800e
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2023-12-27 23:49:33 +0500

    Add stripe  payment for  users sign-up

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,4 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['STRIP_TEST_PUBLISHABLE_KEY'],
   :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
-Strip.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
